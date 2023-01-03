9c5a11100f50385987752d55ef96e88b93eea6fb
wfrsk <wfrsk@proton.me>
2023-01-03 01:50:49 +0100

    add init script

---
+++ perform_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+[ $UID != 0 ] && echo "for agetty setup, run as superuser."
+
+current_directory=$( dirname "$( pwd -L )/${0}" )
+
+ln -S "-old" -sfv $current_directory/util $HOME
+
+for setup_script in `/bin/find config -type f -name "perform_setup.sh"`; do
+  [ -f $setup_script ] && . $setup_script
+done
